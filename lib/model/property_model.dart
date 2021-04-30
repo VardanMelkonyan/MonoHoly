03794983cc595ae5d5a405255e77343f46d8fced
Vardan Melkonyan <vardan.melkonyan@tumo.org>
2021-05-01 01:44:52 +0400

    Override == and toString methods

--- lib/model/property_model.dart
+++ lib/model/property_model.dart
@@ -3,4 +3,19 @@ class PropertyModel {
   final String title;
 
   PropertyModel({this.id, this.title});
+
+  @override
+  String toString() {
+    return title;
+  }
+
+  @override
+  int get hashCode => super.hashCode;
+
+  @override
+  bool operator ==(Object other) {
+    if (super != other) return false;
+    PropertyModel o = other;
+    return o.id == this.id;
+  }
 }
